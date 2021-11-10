1c252ec6a128a32e2aac4d9996e8874e697857c1
PanghoChao <87929190+PanghoChao@users.noreply.github.com>
2021-11-10 15:42:01 +0900

    Add files via upload

---
+++ java1/HelloWorld/src/org/opentutorails/javatutorials/io/InputDemo.java
@@ -0,0 +1,8 @@
+package org.opentutorails.javatutorials.io;
+
+public class InputDemo {
+	 public static void main(String[] args) {
+	        System.out.println(args.length);
+	    }
+
+}
---
+++ java1/HelloWorld/src/org/opentutorails/javatutorials/method/MethodDemo4.java
@@ -0,0 +1,14 @@
+package org.opentutorails.javatutorials.method;
+
+public class MethodDemo4 {
+	
+	  public static String[] getMembers() {
+	        String[] members = { "ÀÌ»ó±Ç", "±è¼ö¹Î", "Á¤ÁØ¿ø" };
+	        return members;
+	    }
+	
+    public static void main(String[] args) {
+        System.out.println(getMembers());
+    }
+
+}
