32efcc362c9cebca52a6c503829136a71d57f579
PanghoChao <87929190+PanghoChao@users.noreply.github.com>
2021-11-19 17:46:48 +0900

    Add files via upload

---
+++ java1/HelloWorld/src/org/opentutorails/javatutorials/overloading/OverloadingDemo2.java
@@ -0,0 +1,19 @@
+package org.opentutorails.javatutorials.overloading;
+
+public class OverloadingDemo2 extends OverloadingDemo {
+	 
+	void A (String arg1, String arg2){
+		System.out.println("sub class : void A (String arg1, String arg2)");
+		} // 오버로딩
+	
+	void A (){System.out.println("sub class : void A ()");} //오버라이딩
+    
+	public static void main(String[] args) {
+	        OverloadingDemo2 od = new OverloadingDemo2();
+	        od.A(); //오버라이딩
+	        od.A(1); //상속
+	        od.A("coding everybody"); //상속
+	        od.A("coding everybody", "coding everybody"); // 오버로딩
+	         
+	    }
+}
